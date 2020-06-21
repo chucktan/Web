af36903149ee9c8427a762c91d43cd885954c737
chuck <724226368@qq.com>
2020-06-22 07:10:06 +0800

    2020.06.22

---
+++ foodie-dev/foodie-dev-api/src/main/java/com/imooc/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.imooc.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//@Controller
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public Object hello(){
+        return "Hello World~";
+    }
+
+
+}
